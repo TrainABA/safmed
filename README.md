ab93ecfb14f5a5d08b90c04e7c0649a3f7b8ff0c
The Madmin <madmin@tuta.io>
2017-04-22 18:12:15 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TrainABA SAFMED App
+
+This is the README file for Theisen's SAFMED App for TrainABA. 
+
+It was developed by Benjamin Theisen as front-end JavaScript-CSS3-HTML5 app. Version one released without a database. This is free and open source software, protected under the MIT license. 
+
+##License here
+
+Theisen's SAFMED App for TrainABA contains definitions for hundreds of scientific terms. These are formally referenced in the TrainABA Supervision Curriculum Series. The book is covered under Creative Commons 4.0 - Attribution - Sharealike license. 
+
---
+++ js/cards.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+//BEGIN Button is 000
+  $(".show-000").click(function() {
+    $(".000").show(300);
+  });
+
+
+//number safmeds individually starting from 001
+  $(".show-001").click(function() {
+    $(".001").show();
+  });
+
+  $(".show-002").click(function() {
+    $(".002").show();
+  });
+
+  $(".show-003").click(function() {
+    $(".003").show();
+  });
+
+  $(".show-004").click(function() {
+    $(".004").show();
+  });
+  $(".show-005").click(function() {
+    $(".005").show();
+  });
+  $(".show-006").click(function() {
+    $(".006").show();
+  });
+  $(".show-007").click(function() {
+    $(".007").show();
+  });
+  $(".show-008").click(function() {
+    $(".008").show();
+  });
+  $(".show-009").click(function() {
+    $(".009").show();
+  });
+  $(".show-010").click(function() {
+    $(".010").show();
+  });
+  $(".show-011").click(function() {
+    $(".011").show();
+  });
+  $(".show-012").click(function() {
+    $(".012").show();
+  });
+  $(".show-013").click(function() {
+    $(".013").show();
+  });
+  $(".show-014").click(function() {
+    $(".014").show();
+  });
+  $(".show-015").click(function() {
+    $(".015").show();
+  });
+  $(".show-016").click(function() {
+    $(".016").show();
+  });
+  $(".show-017").click(function() {
+    $(".017").show();
+  });
+  $(".show-018").click(function() {
+    $(".018").show();
+  });
+  $(".show-019").click(function() {
+    $(".019").show();
+  });
+  $(".show-020").click(function() {
+    $(".020").show();
+  });
+  $(".show-021").click(function() {
+    $(".021").show();
+  });
+
+});
